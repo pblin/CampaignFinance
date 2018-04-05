735bfd6b5f2c7b75acdb5a142186e1f9fb0e5b06
Lin, Bernard <bernardlin@outlook.com>
2018-04-05 12:47:02 -0400

    supplier bidding

---
+++ SupplierBidding/app/javascripts/app_old.js
@@ -0,0 +1,36 @@
+import "../stylesheets/app.css";
+import { default as Web3} from 'web3';
+import { default as contract } from 'truffle-contract';
+import voting_artifacts from '../../build/contracts/Voting.json'
+var Voting = contract(voting_artifacts);
+
+window.startBidding = function() {
+  let timeLimit=$("#timeLimit").val();
+  $("#msg").html("The contarct is being deployed")
+  $("#timeLimit").val("");
+  Voting.deployed().then(function(contractInstance) {return contractInstance.startBidding(timeLimit, {from:web3.eth.accounts[0]})});
+}
+
+window.Bid = function() {
+  let BiddingID=$("#BiddingID").val();
+  let BidHash=$("#BidHash").val();
+  let BidAmount=$("#BidAmount").val();
+  $("#msg").html("The Bid is being deployed")
+Voting.deployed().then(function(contractInstance) {return contractInstance.Bid(BiddingID,BidHash,BidAmount,{from:web3.eth.accounts[0]})});
+}
+$( document ).ready(function() {
+  if (typeof web3 !== 'undefined') {
+    console.warn("Using web3 detected from external source like Metamask")
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }
+
+  Voting.setProvider(web3.currentProvider);
+
+});
+
+
